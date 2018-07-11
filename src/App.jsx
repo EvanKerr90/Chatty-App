35066a4951ed85a94844f9ec4b13f926d728de80
Evan Kerr <evankrkerr90@gmail.com>
2018-07-11 18:54:45 +0000

    completed sending and receiving messages from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx'
-const shortid = require('shortid');
 
 export default class App extends Component {
   constructor() {
   super()
   this.state = {currentUser: {name: "Bob"},
-  messages: [
-    { 
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-      id: shortid.generate()
-    },
-    {
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-      id: shortid.generate()
-    }
-  ]
+  messages: []
 }
 this.newMessage = this.newMessage.bind(this)
 this.newCurrentUser = this.newCurrentUser.bind(this)
+this.socket = new WebSocket('ws://localhost:3001')
 }
+
 
 newCurrentUser(username) {
 this.setState({currentUser:{name: username}})
 }
 
+
 newMessage(content) {
-  const newMessage = {id: shortid.generate(), username: this.state.currentUser.name, content: content};
-  const messages = this.state.messages.concat(newMessage)
-   this.setState({messages: messages})
+   let newMessage = JSON.stringify({username: this.state.currentUser.name, content: content});
+   this.socket.send(newMessage)
+    }
+
+    
+
+componentDidMount() {
+ 
+
+ this.socket.onopen = (event) => {
+     console.log('Connected to server');
+ }
+  this.socket.onmessage = (event) => {
+    let messages = this.state.messages.concat(JSON.parse(event.data))
+     this.setState({messages: messages})
+  }
 }
-
-
 
   render() {
     return (
@@ -43,7 +45,7 @@ newMessage(content) {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser} newMessage={this.newMessage} newCurrentUser={this.newCurrentUser}/>
+        <ChatBar ws={this.socket} currentUser={this.state.currentUser} newMessage={this.newMessage} newCurrentUser={this.newCurrentUser}/>
       </div>
       )
       }
