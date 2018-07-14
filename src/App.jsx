c7ec99f6e9f1cca8c048a552e13b24478906d5ef
Evan Kerr <evankrkerr90@gmail.com>
2018-07-14 03:47:08 +0000

    almost final

--- src/App.jsx
+++ src/App.jsx
@@ -5,103 +5,139 @@ import MessageList from './MessageList.jsx'
 
 export default class App extends Component {
   constructor() {
-  super()
-  this.state = {currentUser: {name: "Anonymous"},
-  messages: [],
-  userCount: '0',
-  appColor: 'red'
-}
-this.newMessage = this.newMessage.bind(this)
-this.newCurrentUser = this.newCurrentUser.bind(this)
-this.imageTest = this.imageTest.bind(this)
-this.newAppColor = this.newAppColor.bind(this)
-this.newUserColor = this.newUserColor.bind(this)
-this.randomColor = this.randomColor.bind(this)
-this.socket = new WebSocket('ws://localhost:3001')
-}
-
-randomColor() {
-  return "#"+((1<<24)*Math.random()|0).toString(16)
-}
-  
-
-newAppColor(color) {
-  if (color === 'random') {
-  this.setState({appColor: this.randomColor()}) 
-  } else {
-  this.setState({appColor: color})
+    super()
+    this.state = {
+      currentUser: {
+        name: "Anonymous"
+      },
+      messages: [],
+      userCount: '0',
+      appColor: 'red'
+    }
+    this.newMessage = this.newMessage.bind(this)
+    this.newCurrentUser = this.newCurrentUser.bind(this)
+    this.newAppColor = this.newAppColor.bind(this)
+    this.newUserColor = this.newUserColor.bind(this)
+    this.socket = new WebSocket('ws://localhost:3001')
   }
-}
 
-newUserColor(color) {
-  if (color === 'random') {
-    this.setState(prevState => ({currentUser: {name: prevState.currentUser.name, color: this.randomColor()}}))
-  } else {
-    this.setState(prevState => ({currentUser: {name: prevState.currentUser.name, color: color}}))
+  randomColor() {
+    return "#" + ((1 << 24) * Math.random() | 0).toString(16)
   }
-}
 
 
-newCurrentUser(username) {
-let content = this.state.currentUser.name + " has changed their name to " + username + "."
-let newMessage = JSON.stringify({type: 'postNotification', content: content})
-this.socket.send(newMessage)
-this.setState(prevState => ({currentUser: {name: username, color: prevState.currentUser.color}}))
-}
-
- imageTest(contentString) {
-  const testRegex = /(jpe?g|gif|png)(?=\?.+|$)/
-  const contentArray = contentString.split(' ')
-  let url = []
-  let newContentString = []
-   contentArray.forEach(function(word) {
-     if (testRegex.test(word)) {
-       url.push(word)
-   } else {
-    newContentString.push(word)
-   }
-  })
-  if (url.length > 0) {
-    return {
-      url: url,
-     content: newContentString.join(' '),
-   }
-  } else {
-  return contentString;
- }
-}
-
-
-newMessage(content) {
-  let imageUrl = this.imageTest(content)
-  if (imageUrl.url) {
-  let newMessage = JSON.stringify({username: this.state.currentUser.name, content: imageUrl.content, type: "postMessage", color: this.state.currentUser.color, images: imageUrl.url});
-  console.log(imageUrl)
-   this.socket.send(newMessage)
+  newAppColor(color) {
+    if (color === 'random') {
+      this.setState({
+        appColor: this.randomColor()
+      })
     } else {
-      console.log("NOT ENTERING RIGHT ONE")
-    let newMessage = JSON.stringify({username: this.state.currentUser.name, content: imageUrl, type: "postMessage", color: this.state.currentUser.color});
+      this.setState({
+        appColor: color
+      })
+    }
+  }
+
+  newUserColor(color) {
+    if (color === 'random') {
+      this.setState(prevState => ({
+        currentUser: {
+          name: prevState.currentUser.name,
+          color: this.randomColor()
+        }
+      }))
+    } else {
+      this.setState(prevState => ({
+        currentUser: {
+          name: prevState.currentUser.name,
+          color: color
+        }
+      }))
+    }
+  }
+
+
+  newCurrentUser(username) {
+    let content = this.state.currentUser.name + " has changed their name to " + username + "."
+    let newMessage = JSON.stringify({
+      type: 'postNotification',
+      content: content
+    })
     this.socket.send(newMessage)
+    this.setState(prevState => ({
+      currentUser: {
+        name: username,
+        color: prevState.currentUser.color
+      }
+    }))
+  }
+
+  imageTest(contentString) {
+    const testRegex = /(jpe?g|gif|png)(?=\?.+|$)/
+    const contentArray = contentString.split(' ')
+    let url = []
+    let newContentString = []
+    contentArray.forEach(function (word) {
+      if (testRegex.test(word)) {
+        url.push(word)
+      } else {
+        newContentString.push(word)
+      }
+    })
+    if (url.length > 0) {
+      return {
+        url: url,
+        content: newContentString.join(' '),
+      }
+    } else {
+      return contentString;
     }
   }
 
 
-componentDidMount() {
- 
-
- this.socket.onopen = (event) => {
-     console.log('Connected to server');
- }
-  this.socket.onmessage = (event) => {
-    let data = JSON.parse(event.data)
-     if (data.type === 'userCount') {
-      this.setState({userCount: data.size})
-    } else if (data.type === 'color') {
-      this.newUserColor(data.color)
+  newMessage(content) {
+    let imageUrl = this.imageTest(content)
+    if (imageUrl.url) {
+      let newMessage = JSON.stringify({
+        username: this.state.currentUser.name,
+        content: imageUrl.content,
+        type: "postMessage",
+        color: this.state.currentUser.color,
+        images: imageUrl.url
+      });
+      this.socket.send(newMessage)
     } else {
-    let messages = this.state.messages.concat(data)
-      this.setState({messages: messages})
+      let newMessage = JSON.stringify({
+        username: this.state.currentUser.name,
+        content: imageUrl,
+        type: "postMessage",
+        color: this.state.currentUser.color
+      });
+      this.socket.send(newMessage)
     }
+  }
+
+
+  componentDidMount() {
+
+
+    this.socket.onopen = (event) => {
+      console.log('Connected to server');
+    }
+    this.socket.onmessage = (event) => {
+      let data = JSON.parse(event.data)
+      if (data.type === 'userCount') {
+        this.setState({
+          userCount: data.size
+        })
+      } else if (data.type === 'color') {
+        this.newUserColor(data.color)
+      } else {
+        let messages = this.state.messages.concat(data)
+        this.setState({
+          messages: messages
+        })
+      }
     }
   }
 
@@ -113,8 +149,8 @@ componentDidMount() {
           <a href="/" className="navbar-brand" >Chatty</a>
           <span className="user-count">{this.state.userCount} users online</span>
         </nav>
-        <MessageList messages={this.state.messages} fontColour={this.state.fontColor}/>
-        <ChatBar fontColour={this.state.fontColor} currentUser={this.state.currentUser.name} newFontColor={this.newFontColor} newUserColor={this.newUserColor} newAppColor={this.newAppColor} newMessage={this.newMessage} appColor={this.state.appColor} newCurrentUser={this.newCurrentUser}/>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser.name} newUserColor={this.newUserColor} newAppColor={this.newAppColor} newMessage={this.newMessage} appColor={this.state.appColor} newCurrentUser={this.newCurrentUser}/>
       </div>
       )
       }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 
 export default class ChatBar extends Component {
     constructor(props) {
-    super(props)
-    this.state = { message: '', username: this.props.currentUser.name, color: this.props.appColor, buttonColor: 'white'}
-    this.onKeyUpMessage = this.onKeyUpMessage.bind(this)
-    this.onKeyUpUsername = this.onKeyUpUsername.bind(this)
-    this.onChangeMessage = this.onChangeMessage.bind(this)
-    this.onChangeUsername = this.onChangeUsername.bind(this)
-    this.onClickColor = this.onClickColor.bind(this)
-    this.onClickSelect = this.onClickSelect.bind(this)
+        super(props)
+        this.state = {
+            message: '',
+            username: this.props.currentUser.name,
+            color: this.props.appColor,
+            buttonColor: 'white'
+        }
+        this.onKeyUpMessage = this.onKeyUpMessage.bind(this)
+        this.onKeyUpUsername = this.onKeyUpUsername.bind(this)
+        this.onChangeMessage = this.onChangeMessage.bind(this)
+        this.onChangeUsername = this.onChangeUsername.bind(this)
+        this.onClickColor = this.onClickColor.bind(this)
+        this.onClickSelect = this.onClickSelect.bind(this)
     }
 
     onChangeMessage(event) {
         event.preventDefault()
         this.setState({
             message: event.target.value
-        }) 
+        })
     }
 
     onChangeUsername(event) {
         event.preventDefault()
         this.setState({
             username: event.target.value
-        }) 
+        })
     }
 
     onKeyUpMessage(event) {
@@ -33,8 +38,10 @@ export default class ChatBar extends Component {
                 alert("You can not submit empty messages")
                 return;
             }
-        this.props.newMessage(this.state.message) 
-        this.setState({message: ''})
+            this.props.newMessage(this.state.message)
+            this.setState({
+                message: ''
+            })
         }
     }
 
@@ -42,12 +49,12 @@ export default class ChatBar extends Component {
         let username = '';
         event.preventDefault();
         if (event.keyCode === 13) {
-        if (this.state.username.length <= 0) {
-         username = "Anonymous";
-         } else {
-         username = this.state.username;
-         }
-        this.props.newCurrentUser(username) 
+            if (this.state.username.length <= 0) {
+                username = "Anonymous";
+            } else {
+                username = this.state.username;
+            }
+            this.props.newCurrentUser(username)
         }
     }
 
@@ -55,42 +62,58 @@ export default class ChatBar extends Component {
     onClickColor(event) {
         event.preventDefault()
         if (this.state.buttonColor.buttonAppColor === 'red') {
-        this.props.newAppColor(event.target.value)
+            this.props.newAppColor(event.target.value)
         } else {
             this.props.newUserColor(event.target.value)
-        } 
-}
+        }
+    }
 
     onClickSelect(event) {
         event.preventDefault()
         if (event.target.value === 'app color') {
-        this.setState(prevState => ({...prevState, buttonColor: {buttonAppColor: 'red', buttonUserColor: 'white', buttonFontColour: 'white'}}))
+            this.setState(prevState => ({ ...prevState,
+                buttonColor: {
+                    buttonAppColor: 'red',
+                    buttonUserColor: 'white',
+                    buttonFontColour: 'white'
+                }
+            }))
         } else {
-            this.setState(prevState => ({...prevState, buttonColor: {buttonAppColor: 'white', buttonUserColor: 'red', buttonFontColor: 'white'}}))
+            this.setState(prevState => ({ ...prevState,
+                buttonColor: {
+                    buttonAppColor: 'white',
+                    buttonUserColor: 'red',
+                    buttonFontColor: 'white'
+                }
+            }))
         }
     }
 
     render() {
     return (
-    <footer className="chatbar" style={{backgroundColor: this.props.appColor}}>
-    <input type="text" className="chatbar-username" onChange={this.onChangeUsername} onKeyUp={this.onKeyUpUsername} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)"/>
-    <input type="text" className="chatbar-message" value={this.state.message} placeholder="Type a message and hit ENTER" onChange={this.onChangeMessage} onKeyUp={this.onKeyUpMessage}/>
+<footer className="chatbar" style={{backgroundColor: this.props.appColor}}>
+    <input type="text" className="chatbar-username" onChange={this.onChangeUsername} onKeyUp={this.onKeyUpUsername} defaultValue={this.props.currentUser.name}
+        placeholder="Your Name (Optional)" />
+    <input type="text" className="chatbar-message" value={this.state.message} placeholder="Type a message and hit ENTER" onChange={this.onChangeMessage}
+        onKeyUp={this.onKeyUpMessage}/>
     <div className="buttons">
-    <span className="color-buttons">
-    <input type="button" className="color-button" value="red" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="blue" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="green" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="yellow" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="black" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="white" onClick={this.onClickColor}/>
-    <input type="button" className="color-button" value="random" onClick={this.onClickColor}/>
-    </span>
-    <span className="change-buttons">
-    <input type="button" className="change-button" style={{backgroundColor: this.state.buttonColor.buttonAppColor}} value="app color" onClick={this.onClickSelect}/>
-    <input type="button" className="change-button" style={{backgroundColor: this.state.buttonColor.buttonUserColor}} value="username color" onClick={this.onClickSelect}/>
-    </span>
+        <span className="color-buttons">
+            <input type="button" className="color-button" value="red" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="blue" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="green" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="yellow" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="black" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="white" onClick={this.onClickColor}/>
+            <input type="button" className="color-button" value="random" onClick={this.onClickColor}/>
+        </span>
+        <span className="change-buttons">
+            <input type="button" className="change-button" style={{backgroundColor: this.state.buttonColor.buttonAppColor}} value="app color"
+                onClick={this.onClickSelect}/>
+            <input type="button" className="change-button" style={{backgroundColor: this.state.buttonColor.buttonUserColor}} value="username color"
+                onClick={this.onClickSelect}/>
+        </span>
     </div>
-  </footer>
+</footer>
     )
 }
 }
